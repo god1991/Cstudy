f28defc8f5b63ad715a3208ef4934c807db05dcf
minyang <506378386@qq.com>
2016-09-18 18:02:10 +0800

    初步

--- main.c
+++ main.c
@@ -6,11 +6,11 @@ struct node
 };
 int main(void)
 {
-   struct node a;
-    a.value=3;
-  struct  node b;
-    b.value=4;
-    a.next=&b;
-    printf("a.netx value is : %d",(*(a.next)).value);
+    struct node a;
+    a.value = 3;
+    struct node b;
+    b.value = 4;
+    a.next = &b;
+    printf("a.netx value is : %d", (*(a.next)).value);
     getchar();
 }
